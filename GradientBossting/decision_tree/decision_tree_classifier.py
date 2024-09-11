96fae05ef3a98544d0d4ca1376104c5571327d1c
Yaroslav Sergaev <105156233+adelardw@users.noreply.github.com>
2024-09-11 23:13:46 +0300

    Update decision_tree_classifier.py

--- GradientBossting/decision_tree/decision_tree_classifier.py
+++ GradientBossting/decision_tree/decision_tree_classifier.py
@@ -4,121 +4,137 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, y=None,
-                       l=None,
-                       r=None, 
-                       split_val=None,
-                       split_feature=None):
-
-        self.y = y
-        self.l = l
-        self.r = r
+    def __init__(self, left , right, split_val, feature):
+        self.left = left
+        self.right = right
         self.split_val = split_val
-        self.split_feature = split_feature
-
-        if self.y is not None:
-            unique, counts = np.unique(self.y, return_counts = True)
-            self.prob = dict(zip(unique, counts / self.y.size))
-        
+        self.feature = feature
 
 
-class DecisionTreeClassifier:
+class Leaf:
+    def __init__(self, y):
+        labels, counts = np.unique(y, return_counts=True)
+
+        self.prob = dict(zip(labels, counts / y.size))
+
+
+class Module:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict_proba(self):
+        pass
+
+    def predict(self):
+        super().__call__()
+        pass
+
+class DecisionTreeClassifier(Module):
     def __init__(self, max_depth, min_leaf_samples, criterion):
+        super().__init__()
+
         self.max_depth = max_depth
         self.min_leaf_samples = min_leaf_samples
         self.criterion = gini if criterion == 'gini' else entropy
+        self.mode = mode
 
-    def search_max_impurity_feature(self, X, y):
+
+    def __max_gain(self, X, y):
+
         num_features = X.shape[1]
+        max_gain = -np.inf
 
-        max_impurity = -np.inf
         for i in range(num_features):
-            feature = X[:, i]
-            split_values = np.unique(feature)
-            max_impurity_feature = -np.inf
+            x = X[:, i]
+            split_values = np.unique(x)
+            max_gain_features = -np.inf
+
             for split_val in split_values:
-                left_y = y[feature < split_val]
-                right_y = y[feature >= split_val]
-                impurity_result = impurity(left_y, right_y, self.criterion)
 
-                if impurity_result > max_impurity_feature:
-                    max_impurity_feature = impurity_result
-                    best_split = split_val
+                mask = x < split_val
+                left_y = y[mask]
+                right_y = y[~mask]
                 
-            if max_impurity_feature > max_impurity:
-                max_impurity = max_impurity_feature
-                best_split_value = best_split
-                best_split_feature = i
+                gain_result = gain(left_y, right_y, self.criterion)
+
+                if gain_result > max_gain_features:
+                    max_gain_features = gain_result
+                    best_split = split_val
             
-        return best_split_value, best_split_feature
-    
-    def build_tree(self, X, y ,depth=0):
+
+            if max_gain_features > max_gain:
+                max_gain = max_gain_features
+                feature = i
+                split_value = best_split
+        
+
+        return max_gain, feature, split_value
+
+
+    def __build_tree(self, X , y, depth = 0):
         depth += 1
+        if len(y) >= self.min_leaf_samples:
+            max_gain, feature, split_value =self.__max_gain(X, y)
 
-        if X.shape[0] >= self.min_leaf_samples:
+            mask = X[:, feature] < split_value
 
-            best_split_value, best_split_feature = self.search_max_impurity_feature(X, y)
-            
-            mask = X[:, best_split_feature] < best_split_value
-
-            left_X = X[mask]
-            right_X = X[~mask]
             left_y = y[mask]
             right_y = y[~mask]
+            left_X = X[mask]
+            right_X = X[~mask]
 
-            if self.max_depth is not None:
-                if len(left_X) == 0 or len(right_X) == 0 or depth >=self.max_depth:
-                    return Node(y=y,split_val=best_split_value,split_feature=best_split_feature)
-            
-            else:
+            if self.max_depth is None:
                 if len(left_X) == 0 or len(right_X) == 0:
-                    return Node(y=y,split_val=best_split_value,split_feature=best_split_feature)
+                    return Leaf(y)
+            else:
+                if len(left_X) == 0 or len(right_X) == 0 or depth >= self.max_depth:
+                    return Leaf(y)
             
-
-
-            return Node(l=self.build_tree(left_X, left_y, depth),
-                        r=self.build_tree(right_X, right_y, depth),
-                        split_val=best_split_value,
-                        split_feature=best_split_feature)
+            return Node(left=self.__build_tree(left_X, left_y, depth),
+                        right=self.__build_tree(right_X, right_y, depth),
+                        split_val=split_value,
+                        feature=feature)
         
-        return Node(y=y)
-    
+        return Leaf(y)
+
     def fit(self, X, y):
         self.X = X
         self.y = y
         self.labels = np.unique(self.y)
-        self.tree = self.build_tree(self.X, self.y, 0)
+        self.tree = self.__build_tree(self.X, self.y, 0)
 
-    def tree_traveling(self, X, indecies, tree, proba):
+
+    def __tree_climb(self, X, indecies, proba, node_or_leaf):
         
-        if tree.y is not None:
+        if isinstance(node_or_leaf, Leaf):
+            leaf = node_or_leaf
             for i in indecies:
-                proba[i] = tree.prob
+                proba[i] = leaf.prob
 
             return proba
-        else:
-            
-            mask = X[:, tree.split_feature] < tree.split_val
 
+        else:
+            node = node_or_leaf
+
+            mask = X[:, node.feature] < node.split_val
             left_X = X[mask]
             right_X = X[~mask]
             left_indecies = indecies[mask]
             right_indecies = indecies[~mask]
 
-            self.tree_traveling(left_X,left_indecies, tree.l, proba) 
-            self.tree_traveling(right_X,right_indecies, tree.r, proba) 
+            self.__tree_climb(left_X, left_indecies, proba, node.left)
+            self.__tree_climb(right_X, right_indecies, proba, node.right)
         
         return proba
-
-
-    def predict_proba(self, X):
-
-        proba = [dict.fromkeys(self.labels)] * X.shape[0]
-        return self.tree_traveling(X, np.arange(X.shape[0]), self.tree, proba)
     
+    def predict_proba(self, X):
+        proba = [dict.fromkeys(self.labels)] * X.shape[0]
 
+        return self.__tree_climb(X, np.arange(X.shape[0]), proba, self.tree)
+    
     def predict(self, X):
-
-        out = self.predict_proba(X)
-        return np.array([max(p, key = lambda k:p[k]) for p in out])
+        return np.array([max(p, key= lambda k: p[k]) for p in self.predict_proba(X)])
     
